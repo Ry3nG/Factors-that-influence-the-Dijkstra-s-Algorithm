ee2adce714a11052cd0c437961b949bb6f52536a
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-23 23:46:06 +0800

    <test> tested AdjListGraph and AdjMatrixGraph

--- src/App.java
+++ src/App.java
@@ -1,12 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Graph g = new Graph(5);
-        g.addEdge(0, 1);
-        g.addEdge(0, 4);
-        g.addEdge(1, 2);
-        g.addEdge(1, 3);
-        g.addEdge(1, 4);
-        g.addEdge(2, 3);
-        g.printGraph();
+        AdjListGraph g1 = new AdjListGraph(5);
+        g1.addEdge(0, 1);
+        g1.addEdge(0, 4);
+        g1.addEdge(1, 2);
+        g1.addEdge(1, 3);
+        g1.addEdge(1, 4);
+        g1.addEdge(2, 3);
+        g1.printGraph();
+
+        System.out.println();
+        AdjMatrixGraph g2 = new AdjMatrixGraph(5);
+        g2.addEdge(0, 1);
+        g2.addEdge(0, 4);
+        g2.addEdge(1, 2);
+        g2.addEdge(1, 3);
+        g2.addEdge(1, 4);
+        g2.addEdge(2, 3);
+        g2.printGraph();
     }
 }
