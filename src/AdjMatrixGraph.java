3d193da2b6d17f3a2759066e24d8f514418e9027
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-24 00:15:08 +0800

    <add> added weighted

--- src/AdjMatrixGraph.java
+++ src/AdjMatrixGraph.java
@@ -1,19 +1,23 @@
 public class AdjMatrixGraph {
+
     int V;
     int matrix[][];
-    public AdjMatrixGraph(int nodes){
+
+    public AdjMatrixGraph(int nodes) {
         V = nodes;
         matrix = new int[V][V];
     }
-    public void addEdge(int src, int dest){
-        matrix[src][dest] = 1;
-        //matrix[dest][src] = 1; //commented because it is a directed graph
+
+    public void addEdge(int src, int dest, int weight) {
+        matrix[src][dest] = weight;
+        // matrix[dest][src] = 1; //commented because it is a directed graph
     }
-    public void printGraph(){
-        for(int i = 0; i < V; i++){
+
+    public void printGraph() {
+        for (int i = 0; i < V; i++) {
             System.out.print("Node " + i + " is connected to: ");
-            for(int j = 0; j < V; j++){
-                if(matrix[i][j] == 1){
+            for (int j = 0; j < V; j++) {
+                if (matrix[i][j] != 0) {
                     System.out.print(j + " ");
                 }
             }
