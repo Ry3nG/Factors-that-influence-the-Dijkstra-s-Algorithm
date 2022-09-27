4e45ec2fe030f512073fb5436dc8f9fcc6367bde
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-27 17:20:37 +0800

    <add> added static class of Edge

--- src/AdjListGraph.java
+++ src/AdjListGraph.java
@@ -1,27 +1,43 @@
-import java.util.*;
-public class AdjListGraph{
-    int V;
-    LinkedList<Edge>[] adjList;
+import java.util.LinkedList;
 
-    public AdjListGraph(int nodes){
-        V = nodes;
-        adjList = new LinkedList[V];
-        for(int i = 0; i < V; i++){
-            adjList[i] = new LinkedList<Edge>();
+public class AdjListGraph {
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
         }
     }
-    public void addEdge(int src, int dest, int weight){
-        Edge edge = new Edge(src, dest, weight);
-        adjList[src].addFirst(edge); //directed graph
+
+    int V;
+    int edgeCount;
+    LinkedList<Edge>[] adjList;
+
+    public AdjListGraph(int vertices) {
+        this.V = vertices;
+        adjList = new LinkedList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjList[i] = new LinkedList<>();
+        }
     }
-    public void printGraph(){
-        for(int i = 0; i < V; i++){
+
+    public void addEdge(int src, int dest, int weight) {
+        Edge edge = new Edge(src, dest, weight);
+        adjList[src].addFirst(edge);
+        edgeCount++;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < V; i++) {
             LinkedList<Edge> list = adjList[i];
-            System.out.print("Node " + i + " is connected to: ");
-            for(int j = 0; j < list.size(); j++){
-                System.out.print(list.get(j).dest + " ");
+            for (int j = 0; j < list.size(); j++) {
+                System.out.println(
+                        "vertex " + i + " is connected to " + list.get(j).dest + " with weight " + list.get(j).weight);
             }
-            System.out.println();
         }
     }
 }
