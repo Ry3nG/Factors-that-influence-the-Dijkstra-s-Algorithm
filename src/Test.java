6a33fda84bb268e859b1ee7dca65fc503c9b8207
ZERUI GONG <Nomlasssssss@outlook.com>
2022-10-03 14:50:03 +0800

    <add> added empiricalTestSparse

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 public class Test {
 
     /**
@@ -10,9 +11,10 @@ public class Test {
 
         // testPrintGraph();
         // testDijkstraArray();
-        // testDijkstraPQ();
+        //testDijkstraPQ();
         // testGenerateRandomGraph(10,10);
-        empiricalTest(300, 1000, 500, 1000);
+        //empiricalTest(300, 1000, 500, 1000);
+        empiricalTestSparse(1000,1000,500,1000);
     }
 
     /**
@@ -59,7 +61,16 @@ public class Test {
     private static void testDijkstraPQ() {
         System.out.println("testDijkstraPQ: ");
         Graph g = new Graph(5);
-        g.addEdge(0, 1, 4);
+        g.addEdge(0,1,3);
+        g.addEdge(0,2,10);
+        g.addEdge(0,3,8);
+        g.addEdge(0,4,11);
+        g.addEdge(1,2,5);
+        g.addEdge(1,3,3);
+        g.addEdge(1,4,2);
+        g.addEdge(2,3,1);
+        g.addEdge(3,4,1);
+        /*g.addEdge(0, 1, 4);
         g.addEdge(0, 2, 2);
         g.addEdge(0, 3, 6);
         g.addEdge(0, 4, 8);
@@ -68,6 +79,7 @@ public class Test {
         g.addEdge(2, 3, 1);
         g.addEdge(3, 1, 1);
         g.addEdge(3, 4, 3);
+        */
         g.printGraph();
         DijkstraAlgo dijkstraTest = new DijkstraAlgo(5);
         dijkstraTest.dijkstraPQStart(5, 0, g.adjList);
@@ -80,7 +92,7 @@ public class Test {
      * @param maxEdges    maximum number of edges
      */
     private static void testGenerateRandomGraph(int maxVertices, int maxWeight) {
-        Graph g = new Graph(maxVertices, maxWeight);
+        Graph g = new Graph(maxVertices, maxWeight, 0);
         System.out.println("Random Graph generated, V = " + g.V + ", E = " + g.E);
         g.printGraph();
     }
@@ -93,9 +105,9 @@ public class Test {
      * empirical test: verticeArr.csv, edgeArr.csv,
      * timeArr(test1).csv, timeArr(test2).csv
      * 
-     * @param maxVertices maximum number of vertices for random graph generation
-     * @param maxWeight   maximum weight for random graph generation
-     * @param graphCount  number of graphs to be generated
+     * @param maxVertices  maximum number of vertices for random graph generation
+     * @param maxWeight    maximum weight for random graph generation
+     * @param graphCount   number of graphs to be generated
      * @param averageTimes number of times to run the algorithm for each graph
      * @throws IOException
      */
@@ -105,7 +117,7 @@ public class Test {
                 timeArr2 = new long[graphCount];
         for (int i = 0; i < graphCount; i++) {
             System.out.println("Generating graph " + (i + 1) + "...");
-            Graph g = new Graph(maxVertices, maxWeight);
+            Graph g = new Graph(maxVertices, maxWeight, 0);
             verticeArr[i] = g.V;
             edgeArr[i] = g.E;
             long timeTotal1 = 0;
@@ -130,6 +142,39 @@ public class Test {
         MakeCSV.CSVprinter(edgeArr, "edgeArr.csv");
         MakeCSV.CSVprinter(timeArr1, "timeArr(test1).csv");
         MakeCSV.CSVprinter(timeArr2, "timeArr(test2).csv");
+    }
+
+    private static void empiricalTestSparse(int maxVertices, int maxWeight, int graphCount, int averageTimes)
+            throws IOException {
+        long[] verticeArr = new long[graphCount], edgeArr = new long[graphCount], timeArr1 = new long[graphCount],
+                timeArr2 = new long[graphCount];
+        for (int i = 0; i < graphCount; i++) {
+            System.out.println("Generating graph " + (i + 1) + "...");
+            Graph g = new Graph(maxVertices, maxWeight, 1);
+            verticeArr[i] = g.V;
+            edgeArr[i] = g.E;
+            long timeTotal1 = 0;
+            long timeTotal2 = 0;
+            for (int j = 0; j < averageTimes; j++) {
+                DijkstraAlgo dijkstraTest = new DijkstraAlgo(g.V);
+                long startTime = System.nanoTime();
+                dijkstraTest.dijkstraArrayStart(g.V, 0, g.adjMatrix);
+                long endTime = System.nanoTime();
+                timeTotal1 += endTime - startTime;
+
+                startTime = System.nanoTime();
+                dijkstraTest.dijkstraPQStart(g.V, 0, g.adjList);
+                endTime = System.nanoTime();
+                timeTotal2 += endTime - startTime;
+            }
+            timeArr1[i] = timeTotal1 / averageTimes;
+            timeArr2[i] = timeTotal2 / averageTimes;
+
+        }
+        MakeCSV.CSVprinter(verticeArr, "verticeArr(Sparse).csv");
+        MakeCSV.CSVprinter(edgeArr, "edgeArr(Sparse).csv");
+        MakeCSV.CSVprinter(timeArr1, "timeArr1(Sparse).csv");
+        MakeCSV.CSVprinter(timeArr2, "timeArr2(Sparse).csv");
     }
 
 }
